3f728c9c3312a11ae8f42963a069d3a5ddebf6e9
ICONICSTAR <84264252+Satish-SaGo@users.noreply.github.com>
2021-06-07 16:11:27 +0530

    Add files via upload

---
+++ dynamic programming-- factorial.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int dp[1000005];
+ int fact(int n)
+{	
+if(n<=1)	
+    return 1;	
+if(dp[n]!=-1)	
+   return dp[n];	 
+return dp[n]=n*fact(n-1); 
+} 
+int main()
+{	
+int n;	
+cout<<"enter a number:";
+cin>>n;	
+for(int i=0;i<=n;i++)
+ { 
+   dp[i]=-1;
+ }	
+cout<<fact(n);
+	return 0;
+}
